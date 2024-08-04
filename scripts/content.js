6a34a7bd7c9a4aac24e89d9bba6715eaeb181206
Keroz-WD <kerozwebdesign@gmail.com>
2024-08-05 00:34:06 +0200

    Function highlight added

--- scripts/content.js
+++ scripts/content.js
@@ -2,8 +2,35 @@
 
 console.log("Test content.js : OK");
 
-let h1 = document.querySelector("h1").textContent;
+const titlesList = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+
+// const displayTitles = () => {
+//   titlesList.forEach((node) => {
+//     console.log(node.nodeName);
+//     console.log(node.textContent);
+//   });
+// };
+
+const list = () => {
+  const obj = {};
+};
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  sendResponse(h1);
+  sendResponse(titlesList);
+  console.log(titlesList);
+  highlightTitles(titlesList);
+  getTitlesContent(titlesList);
 });
+
+// Set titles' background in yellow
+const highlightTitles = (titles) => {
+  titles.forEach((element) => {
+    element.style.backgroundColor = "yellow";
+  });
+};
+
+const getTitlesContent = (titles) => {
+  titles.forEach((element) => {
+    console.log(element.textContent);
+  });
+};
--- scripts/popup.js
+++ scripts/popup.js
@@ -2,19 +2,18 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const startButton = document.getElementById("startButton");
+
   function display_h1(h1) {
     document.getElementById("h1").innerHTML = h1;
   }
-
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    display_h1(message);
-  });
 
   startButton.addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, { data: "hello" }, (response) => {
         if (response) {
-          display_h1(response);
+          console.log(response);
+        } else {
+          // Catch error
         }
       });
     });
